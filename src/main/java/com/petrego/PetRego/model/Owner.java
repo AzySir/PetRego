5ea882992b882a08a8b94b8bd1b2fb043d2014a2
AzySir <azy.sir@gmail.com>
2019-09-10 16:06:40 +1000

    Added in new pets variable

--- src/main/java/com/petrego/PetRego/model/Owner.java
+++ src/main/java/com/petrego/PetRego/model/Owner.java
@@ -11,20 +11,30 @@ public class Owner {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+
+    //Owner first name
     @Column(name = "first_name", nullable = false)
     private String first_name;
 
+    //Owner last name
     @Column(name = "last_name", nullable = false)
     private String last_name;
 
+    //Owner Date of Birth
     @Column(name = "dob", nullable = false)
     private String dob;
 
+    //Owners Email
     @Column(name = "email", nullable = false)
     private String email;
 
+    //Owners Phone Number
     @Column(name = "phone_number", nullable = false)
     private Long phone_number;
 
-    
+    //Owners Pets Id
+    @Column(name = "pet", nullable = false)
+    private String pets;
+
+
 }
