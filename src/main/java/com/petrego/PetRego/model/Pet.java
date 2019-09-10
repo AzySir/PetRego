d9520772112463f9f9b9e077a65a1721abd55d48
AzySir <azy.sir@gmail.com>
2019-09-10 12:38:10 +1000

    Added JPA for Database and getters and setters for Pet.class

--- src/main/java/com/petrego/PetRego/model/Pet.java
+++ src/main/java/com/petrego/PetRego/model/Pet.java
@@ -1,10 +1,105 @@
 package com.petrego.PetRego.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+
+
+/**
+ * Scope: This class handles the pet information
+ *
+ * @version 1.0
+ * @author Adam Sir
+ * @date 10th September 2019
+ *
+ */
 
 @Entity
 @Table(name = "pets")
+@EntityListeners(Pet.class)
 public class Pet {
+
+
+    //Pet Id for Searching up - use of Api
+    @Id
+    @GeneratedValue(strategy = GenerateType.Auto)
+    private Long id;
+
+    //Pet Name
+    @Column(name = "pet_name", nullable = false)
+    private String pet_name;
+
+    //Type of Pet - Dog / Cat / Chicken / Snake
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    //Food - Bones / Fish / Corn / Mice
+    @Column(name = "food", nullable = false)
+    private String food;
+
+    //Age as part of v2 API
+    @Column(name = "age", nullable = false)
+    private int age;
+
+    //Owner_id to link back to owner. Also a search value via api
+    @Column(name = "owner_id", nullable = false)
+    private Long owner_id;
+
+
+    //<!-------- Constructors ---------->
+    
+
+
+
+
+
+    //<------ Getter and Setters for Above Variables ------>
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPetName() {
+        return pet_name;
+    }
+
+    public void setPetName(String pet_name) {
+        this.pet_name = pet_name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Long getOwnerId() {
+        return owner_id;
+    }
+
+    public void setOwnerId(Long owner_id) {
+        this.owner_id = owner_id;
+    }
+
 
 }
