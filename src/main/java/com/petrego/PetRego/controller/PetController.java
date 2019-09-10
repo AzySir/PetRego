7fb456092656e5f717d608942a1cadbe97c3f8d6
AzySir <azy.sir@gmail.com>
2019-09-10 13:22:19 +1000

    Added get ALL pets

--- src/main/java/com/petrego/PetRego/controller/PetController.java
+++ src/main/java/com/petrego/PetRego/controller/PetController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -16,9 +17,15 @@ public class PetController {
     PetRepository petRepository;
     //Get ALL pets
     @GetMapping("/v1/pets")
+    public List<Pet> getAllPets() {
+        return petRepository.findAll();
+    }
 
     //Get Pet By Id
     @GetMapping("/v1/pet/{id}")
+    public List<Pet> getPetById() {
+        
+    }
 
     //Get Pet(s) By Name
     @GetMapping("/v1/pet/name/{name}")
