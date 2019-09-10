a48fb0e9e505721543c29592eb845c9be6c14cde
AzySir <azy.sir@gmail.com>
2019-09-10 16:17:52 +1000

    Added comments

--- src/main/java/com/petrego/PetRego/repository/OwnerRepository.java
+++ src/main/java/com/petrego/PetRego/repository/OwnerRepository.java
@@ -3,6 +3,14 @@ package com.petrego.PetRego.repository;
 import com.petrego.PetRego.model.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Scope: This interface handles custom JPA Queries accesed via Controllers for Owner
+ *
+ * @version 1.0
+ * @author Adam Sir
+ * @date 10th September 2019
+ *
+ */
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
-    
+
 }
