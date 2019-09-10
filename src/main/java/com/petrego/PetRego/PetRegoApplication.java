887eb04cad9c40d325372d0cd82e2ec82b9d149f
AzySir <azy.sir@gmail.com>
2019-09-10 12:24:31 +1000

    First commit with Project

---
+++ src/main/java/com/petrego/PetRego/PetRegoApplication.java
@@ -0,0 +1,13 @@
+package com.petrego.PetRego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetRegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetRegoApplication.class, args);
+	}
+
+}
